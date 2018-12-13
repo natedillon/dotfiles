d9215ce6f29606288684e37096826fb62752963d
Nate Dillon <natedillon@gmail.com>
2018-12-13 14:15:23 -0600

    Added aliases for apache and local config files

--- scripts/alias.sh
+++ scripts/alias.sh
@@ -13,8 +13,17 @@ alias edit="atom"
 # fab
 alias fab="fab -f conf/fabfile.py"
 
-# hosts
-alias hosts="sudo atom /etc/hosts"
-
 # Composer
 alias composer='php ~/composer.phar'
+
+# Apache
+alias start-apache='sudo apachectl start'
+alias stop-apache='sudo apachectl stop'
+alias restart-apache='sudo apachectl restart'
+
+# Config files
+alias config-apache="sudo atom /etc/apache2/httpd.conf"
+alias config-apache-users="sudo atom /etc/apache2/users/"
+alias config-hosts="sudo atom /etc/hosts"
+alias config-vhosts="sudo atom /private/etc/apache2/extra/httpd-vhosts.conf"
+alias config-php="sudo atom /etc/php.ini"
