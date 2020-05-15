eb4c2ffcfc97950019d8f73b86964d7afad1e931
Nate Dillon <natedillon@gmail.com>
2020-05-15 15:47:25 -0500

    Finished setting up the Java VM installer

--- install/java.sh
+++ install/java.sh
@@ -1,80 +1,122 @@
 #!/bin/bash
 
+set -eo pipefail
+
 source ../functions/colors.sh
 source ../functions/alerts.sh
 
-info "Running K-State Java setup"
+info "Running the K-State Java setup..."
 
-# Ask to install IntelliJ
+# Options
+java_vm_path="$PROJECT_PATH/kstateome/java-vm"
+java_vm_chef_path="$java_vm_path/chef"
+
+# Homebrew
+info "Updating Homebrew..."
+brew update
+
+# Java VM directory
+info "Checking for the Java VM directory..."
+if [ -d "$java_vm_path" ]; then
+  success "The Java VM directory exists"
+else
+  info "Creating the Java VM directory..."
+  mkdir "$java_vm_path"
+fi
+
+# Chef directory
+if [ -d "$java_vm_chef_path" ]; then
+  success "The Chef directory exists"
+else
+  info "Creating the Chef directory..."
+  mkdir "$java_vm_chef_path"
+fi
+
+# Vagrant
+info "Checking for Vagrant..."
+if hash vagrant 2>/dev/null; then
+  success "Vagrant is installed"
+else
+  info "Installing Vagrant..."
+  brew cask install vagrant
+fi
+
+# ChefDK
+info "Checking for ChefDK..."
+if brew ls --versions chefdk 2>/dev/null; then
+  success "ChefDK is installed"
+else
+  info "Installing ChefDK..."
+  brew cask install chef/chef/chefdk
+fi
+
+# Vagrant plugins
+info "Installing Vagrant plugins..."
+vagrant plugin install vagrant-berkshelf
+vagrant plugin install vagrant-omnibus
+
+# ome_chef_data
+info "Checking for the ome_chef_data repository..."
+if [ -d "$java_vm_path/ome_chef_data" ]; then
+  success "The ome_chef_data repository is already cloned"
+else
+  info "Cloning the ome_chef_data repository..."
+  git clone git@github.com:kstateome/ome_chef_data.git $java_vm_path
+fi
+
+# ome_wildfly_cluster
+info "Checking for the ome_wildfly_cluster repository..."
+if [ -d "$java_vm_chef_path/ome_wildfly_cluster" ]; then
+  success "The ome_wildfly_cluster repository is already cloned"
+else
+  info "Cloning the ome_wildfly_cluster repository..."
+  git clone git@github.com:kstateome/ome_wildfly_cluster.git $java_vm_chef_path
+fi
+
+# Symlinks
+create_symlink () {
+  name=$1
+  source=$2
+  link=$3
+  info "Checking for the $name symlink..."
+  if [ -L $link ]; then
+    success "The symlink already exists"
+  elif [ ! -d "$path" ]; then
+    error "Repository not found. Please clone the ome_chef_data repository before creating the symlink."
+  else
+    info "Creating symlink..."
+  fi
+}
+create_symlink data_bags $java_vm_path/ome_chef_data/data_bags $java_vm_path/data_bags
+create_symlink environments $java_vm_path/ome_chef_data/environments $java_vm_path/environments
+create_symlink roles $java_vm_path/ome_chef_data/roles $java_vm_path/roles
+
+# OpenJDK
+if /usr/libexec/java_home -v 2>/dev/null; then
+  success "Java SDK is installed..."
+else
+  info "Installing OpenJDK..."
+  brew cask install adoptopenjdk8
+fi
+
+# Maven
+if hash mvn 2>/dev/null; then
+  success "Maven is installed"
+else
+  info "Installing Maven..."
+  brew install maven
+fi
+
+# IntelliJ IDEA Community Edition
 while true; do
   read -p "Would you like to install IntelliJ IDEA Community Edition? [y/n]: " input
   case $input in
     [yY][eE][sS]|[yY] ) install_intellij=true; break;;
     [nN][oO]|[nN] ) install_intellij=false; break;;
-    * ) echo "Please answer yes (Y/y) or no (N/n).";;
+    * ) echo "Please answer yes [Y/y] or no [N/n].";;
   esac
 done
-echo $install_intellij
-
-# Set Java VM path
-java_vm_path="$PROJECT_PATH/kstateome/java-vm"
-if [ ! -d "$java_vm_path" ]; then
-  mkdir "$java_vm_path"
-fi
-
-# Install Vagrant
-if hash vagrant 2>/dev/null; then
-  echo "Vagrant is already installed"
-else
-  echo "Installing Vagrant"
-fi
-
-# Install ChefDK
-if brew ls --versions chefdk 2>/dev/null; then
-  echo "Java is already installed"
-else
-  echo "Installing ChefDK"
-fi
-
-# Install Vagrant plugins
-
-# Clone the repositories
-
-# Create symlinks
-create_symlink () {
-  source=$1
-  link=$2
-  if [ -L $link ]; then
-    echo "symlink already exists"
-  elif [ ! -d "$path" ]; then
-    echo "Repository not found"
-  else
-    echo "Creating symlink"
-  fi
-}
-create_symlink $java_vm_path/ome_chef_data/data_bags $java_vm_path/data_bags
-create_symlink $java_vm_path/ome_chef_data/environments $java_vm_path/environments
-create_symlink $java_vm_path/ome_chef_data/roles $java_vm_path/roles
-
-# Install OpenJDK
-if /usr/libexec/java_home -v 2>/dev/null; then
-  echo "Java SDK is already installed"
-else
-  echo "Installing OpenJDK"
-fi
-
-# Install Maven
-if hash mvn 2>/dev/null; then
-  echo "Maven is already installed"
-else
-  echo "Installing Maven"
-fi
-
-# Copy Maven settings to user directory
-
-# Install IntelliJ IDEA Community Edition
 if $install_intellij; then
-  echo "Installing IntelliJ IDEA Community Edition"
-else
-  echo "Skipping IntelliJ"
+  info "Installing IntelliJ IDEA Community Edition..."
+  brew cask install intellij-idea-ce
 fi
