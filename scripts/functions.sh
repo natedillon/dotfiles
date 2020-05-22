bb4df046611a99b135a7bdcc8ae52eb09179cf50
Nate Dillon <natedillon@gmail.com>
2020-05-22 15:44:17 -0500

    Moved functions from gitconfig to functions file

--- scripts/functions.sh
+++ scripts/functions.sh
@@ -39,3 +39,35 @@ warning () {
 error () {
   red "$@"
 }
+
+# Get an e-mail address from the user
+get_email () {
+  valid_email=false
+  until $valid_email; do
+    echo
+    read -p "E-mail address: " email
+    if is_valid_email $email; then
+      valid_email=true
+    else
+      warning "Invalid e-mail address"
+    fi
+  done
+}
+
+# Check to see if a string is empty
+is_empty () {
+  if [ -z "$1" ]; then
+    return 0
+  else
+    return 1
+  fi
+}
+
+# Check to see if a string is a valid e-mail address
+is_valid_email () {
+  if echo $1 | grep '^[a-zA-Z0-9._%+-]*@[a-zA-Z0-9]*\.[a-zA-Z0-9]*$' >/dev/null; then
+    return 0
+  else
+    return 1
+  fi
+}
