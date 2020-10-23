8b297594c71a8487fb4d5b263d99a700b754263d
Nate Dillon <natedillon@gmail.com>
2020-10-23 13:29:05 -0500

    Set up PHP in the installer

--- install.sh
+++ install.sh
@@ -264,9 +264,20 @@ dotfiles_installer () {
   sudo apachectl restart
 
 
+  # PHP
+  # -------------------------
+
+  info "Setting up PHP..."
+
+  # Backup existing files
+  if [ -f "/usr/local/etc/php/7.4/php.ini" ]; then
+    info "Making a backup of the existing php.ini file..."
+    cp /usr/local/etc/php/7.4/php.ini $backup_location
+  fi
+
   # Copy PHP config file
-  #backup file
-  #cp config/php/7.4/php.ini /usr/local/etc/php/7.4
+  cp config/php/7.4/php.ini /usr/local/etc/php/7.4
+
 
 
   # MariaDB
