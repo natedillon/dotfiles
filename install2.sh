12dec8533490c237adf5a816a58903c7a3035e59
Nate Dillon <natedillon@gmail.com>
2021-08-21 16:38:28 -0500

    Added install confirmation

--- install2.sh
+++ install2.sh
@@ -5,10 +5,47 @@ set -eo pipefail
 source scripts/functions.sh
 
 echo
-
 yellow "=========================================="
 blue   "  natedillon/dotfiles"
 echo   "  https://github.com/natedillon/dotfiles"
 yellow "=========================================="
+
+
+# Installer function
+# -------------------------
+
+dotfiles_installer () {
+  echo
+  success "Installation complete"
+}
+
+
+# Confirm the user would like to run the installer
+# -------------------------
+
+if [ "$1" == "--force" -o "$1" == "-f" ]; then
+  run_installer=true
+else
+  while true; do
+    echo
+    echo "This may overwrite existing files in your home directory."
+    read -p "Do you want to continue? [y/n]: " input
+    case $input in
+      [yY][eE][sS]|[yY] ) run_installer=true; break;;
+      [nN][oO]|[nN] ) run_installer=false; break;;
+      * ) warning "\nPlease answer yes [Y/y] or no [N/n].";;
+    esac
+  done
+fi
+
+
+# Run the installer
+# -------------------------
+
+if $run_installer; then
+  echo
+  info "Running the installer..."
+  dotfiles_installer
+fi
 
 echo
