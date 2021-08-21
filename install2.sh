bf4124bd2a5437a3a27cc552414b669d44656329
Nate Dillon <natedillon@gmail.com>
2021-08-21 16:05:15 -0500

    Set up the new install script

---
+++ install2.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+set -eo pipefail
+
+source scripts/functions.sh
+
+echo
+
+yellow "=========================================="
+blue   "  natedillon/dotfiles"
+echo   "  https://github.com/natedillon/dotfiles"
+yellow "=========================================="
+
+echo
