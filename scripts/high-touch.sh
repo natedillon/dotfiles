3c1c15e761b56b9c5c326757c50bb0136f55cde4
Nate Dillon <natedillon@gmail.com>
2021-08-27 11:41:09 -0500

    Setting up High Touch setup script

---
+++ scripts/high-touch.sh
@@ -0,0 +1,18 @@
+# High Touch
+# -------------------------
+
+# Homebrew packages
+brew bundle --verbose --no-lock --file="./brewfiles/High-Touch.Brewfile"
+
+# SSH key
+#ssh-keygen -t rsa -b 4096
+
+# SSH config
+
+# SDKMAN!
+#curl -s "https://get.sdkman.io" | bash
+
+# SDKMAN! config for M1
+
+# Java
+#sdk install java 11.0.12-librca
