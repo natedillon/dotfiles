338c028dd852cec0b96cac6d1cf7339ae7544841
Nate Dillon <natedillon@gmail.com>
2021-10-19 13:41:00 -0500

    Added Zsh setup script

---
+++ setup/zsh.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+#
+# Zsh
+#
+
+cd "$(dirname "$0")"
+cd ../functions
+source ./alerts.sh
+cd - > /dev/null
+
+#if [ -f "$HOME/.zshrc" ]; then
+#  info "Making a backup of .zshrc..."
+#  cp $HOME/.zshrc $backup_location
+#fi
+
+info "Adding .zshrc to home directory..."
+cp ../.zshrc $HOME
+#source $HOME/.zshrc
